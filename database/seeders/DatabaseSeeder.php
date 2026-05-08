ea249db0dc9553778223f8f34e27ae0a4e3f5af7
hergimerc <hzsn@hzsn.my.id>
2026-05-08 16:52:03 +0700

    Replace User::factory() with direct User::create() in DatabaseSeeder
    Factories depend on the fake() helper which is unavailable on some shared hosts.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        User::create([
             'name' => 'Eric',
             'email' => 'hzsn@hzsn.my.id',
             'password' => bcrypt('password'),
